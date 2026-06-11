c2d6d5dd82b81c2358f29cd85a4a050250143463
Lilian Kamikazi <l.kamikazi@alustudent.com>
2026-06-11 08:26:12 -0700

    Finalized script

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,15 @@
- #!/bin/bash
+#!/bin/bash
+zipping() {
+echo " interrupt detected "
+echo " cleaning up ..."
+	if [ -d $project_dir ];then
+	tar cf "${project_dir}_archive.tar.gz" "$project_dir"
+	rm -rf "$project_dir"
+	echo "Incompleted structure removed"
+	fi
+	exit 1
+}
+trap zipping  SIGINT
 
 echo "Start deploying."
 echo "Confirming if it python 3 is installed."
@@ -15,31 +26,27 @@ if [ -d $project_dir ];then
    echo "Directory already exists"
 
 else
-
     mkdir -p "$project_dir/Helpers"
     mkdir -p "$project_dir/reports"
 
 echo "Folders created."
-    mv attendance_checker.py "$project_dir/"
-    mv assets.csv            "$project_dir/Helpers/"
-    mv config.json           "$project_dir/Helpers/"
-    mv reports.log           "$project_dir/reports/"
+    cp attendance_checker.py "$project_dir/"
+    cp assets.csv            "$project_dir/Helpers/"
+    cp config.json           "$project_dir/Helpers/"
+    cp reports.log           "$project_dir/reports/"
 
 echo "Files moved"
 
 fi
 
-read -p "Do you want to update the attendance thresholds? (y/n): "input
-if  [ "$choice" = "y" ]; then
+read -p "Do you want to update the attendance thresholds? (y/n): " input
+if  [ "$input" = "y" ]; then
 
-    read -p "Enter warning threshold in %: "warning
-    read -p "Enter failure threshold %: " failure
-    sed -i  "s/75/$warning/" "$name/Helpers/config.json"
-    sed -i  "s/50/$failure/" "$name/Helpers/config.json"
-
-echo " config.json updated with new thresholds"
-
+    read -p "New warning threshold in %: " warning
+    read -p "New failure threshold %: " failure
+    sed -i  "s/75/$warning/" "$project_dir/Helpers/config.json"
+    sed -i  "s/50/$failure/" "$project_dir/Helpers/config.json"
+    echo " config.json updated with new thresholds"
 fi
-    
 echo " Deployment successfully "
 
